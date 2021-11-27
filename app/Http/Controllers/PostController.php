df48090ed29ac1a0f2f8b56ee439d787deb87dec
odanngo <yujioda0622@outlook.jp>
2021-11-27 21:31:35 +0900

    記事の削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Post;
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
+
 
 class PostController extends Controller
 {
-    //
+    public function index(Post $post)
+    {
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('create');
+    }
+    
+    public function store(Post $post, Request $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}', 'PostController@show');
+
+Route::post('/posts', 'PostController@store');
